87991edcad3d78ad4ebd8ecdfa8daf1ddf72a4e1
冯鑫龙 <18862005869@163.com>
2018-09-20 18:21:24 +0800

    增加页面

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,127 @@
 <template>
   <div id="app">
     <!-- 顶部 -->
-        <div class='header'>
-            <div class='header-left'>
+    <div class='header'>
+        <div class='header-left'>
+            <ul>
+                <li><a href="">古道人家</a></li>
+                <li><a href="">古道人家</a></li>
+                <li><a href="">古道人家</a></li>
+                <li><a href="">古道人家</a></li>
+                <li><a href="">古道人家</a></li>
+                <li><a href="">古道人家</a></li>
+            </ul>
+        </div>
+        <div class='header-right'>
+            <div class="imghead">
+                <img src="/static/index/userhead.jpg">
             </div>
-            <div class='header-right'>
-                <ul class='subZhu'>
-                    <li class="li1">
-                        <a id='linkZhu' href='javascript:void(0);'>注册</a>
-                    </li>
-                    <li>
-                        <a id='linkSub' href='javascript:void(0);'>登录</a>
-                    </li>
-                </ul>
-                <div class='head-img' title='头像名称'><img src='public/home/index/img/weixin.jpg' width='50'>
-                    <div class="head-mess">
+            <!-- <ul class='subZhu'>
+                <li class="li1">
+                    <a id='linkZhu' href='javascript:void(0);'>注册</a>
+                </li>
+                <li>
+                    <a id='linkSub' href='javascript:void(0);'>登录</a>
+                </li>
+            </ul>
+            <div class='head-img' title='头像名称'>
+                <div class="head-mess" style="display:none;">
+                    <ul>
+                        <li><a href="#">基本资料</a></li>
+                        <li><a href="#">文章信息</a></li>
+                        <li><a href="#">相册信息</a></li>
+                        <li><a href="#" class="loginOut">退出</a></li>
+                    </ul>
+                </div>
+            </div> -->
+        </div>
+    </div>
+    <!-- <router-view/> -->
+    <div class="content">
+
+    </div>
+<!-- 底部 -->
+    <div class='bottom'>
+        <div class='bannerd'>
+            <div class='bannerdt'>
+                <div class='banttt bant1'>
+                    <a href='javascript:void(0);'>
+                        <h1>
+                            <img src='/static/index/foot.png' />
+                        </h1>
+                    </a>
+                </div>
+                <div class='banttt bant2'>
+                    <div class='lianjie1'>
                         <ul>
-                            <li><a href="#">基本资料</a></li>
-                            <li><a href="#">文章信息</a></li>
-                            <li><a href="#">相册信息</a></li>
-                            <li><a href="#" class="loginOut">退出</a></li>
+                            <li>
+                                <a href='javascript:void(0);'>关于我们</a>|</li>
+                            <li>
+                                <a href='javascript:void(0);'>AI相关</a>|</li>
+                            <li>
+                                <a href='javascript:void(0);'>合作申请</a>|</li>
+                            <li>
+                                <a href='javascript:void(0);'>意见反馈</a>
+                            </li>
                         </ul>
                     </div>
+                    <div class='shenhe'>
+                        <span>由本人亲制</span> 待审核
+                    </div>
+                </div>
+                <div class='banttt bant3'>
+                    <div class='guanzhu'>
+                        <div class='guanwo'>关注我们：</div>
+                        <img src="/static/index/weixin.png" alt=''>
+                    </div>
+                </div>
+            </div>
+            <div class='bannerdb'>
+                <div class='banndbt'>
+                    <div class='zzz'>友情链接：</div>
+                    <ul>
+                        <li>
+                            <a href='javascript:void(0);'>伞兵小队</a>|
+                        </li>
+                        <li>
+                            <a href='javascript:void(0);'>壮哉104</a>|
+                        </li>
+                        <li>
+                            <a href='javascript:void(0);'>都会成为老男孩</a>|
+                        </li>
+                        <li>
+                            <a href='javascript:void(0);'>D350</a>|
+                        </li>
+                        <li>
+                            <a href='javascript:void(0);'>大邳</a>|
+                        </li>
+                        <li>
+                            <a href='javascript:void(0);'>站长之家</a>
+                        </li>
+                        <li>
+                            <a href='admin/index/login.html'>管理员</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class='banndbb'>
+                    Powered by Sanbing v1.0.0 &copy;2017 伞兵一队
                 </div>
             </div>
         </div>
-    <router-view/>
-<!-- 底部 -->
-        <div class='bottom'>
-                <div class='bannerd'>
-                    <div class='bannerdt'>
-                        <div class='banttt bant1'>
-                            <a href='javascript:void(0);'>
-                                <h1>
-                                    <img src='foot.png' />
-                                </h1>
-                            </a>
-                        </div>
-                        <div class='banttt bant2'>
-                            <div class='lianjie1'>
-                                <ul>
-                                    <li>
-                                        <a href='javascript:void(0);'>关于我们</a>|</li>
-                                    <li>
-                                        <a href='javascript:void(0);'>AI相关</a>|</li>
-                                    <li>
-                                        <a href='javascript:void(0);'>合作申请</a>|</li>
-                                    <li>
-                                        <a href='javascript:void(0);'>意见反馈</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class='shenhe'>
-                                <span>由本人亲制</span> 待审核
-                            </div>
-                        </div>
-                        <div class='banttt bant3'>
-                            <div class='guanzhu'>
-                                <div class='guanwo'>关注我们：</div>
-                                <img :src="footImg" alt=''>
-                            </div>
-                        </div>
-                    </div>
-                    <div class='bannerdb'>
-                        <div class='banndbt'>
-                            <div class='zzz'>友情链接：</div>
-                            <ul>
-                                <li>
-                                    <a href='javascript:void(0);'>伞兵小队</a>|
-                                </li>
-                                <li>
-                                    <a href='javascript:void(0);'>壮哉104</a>|
-                                </li>
-                                <li>
-                                    <a href='javascript:void(0);'>都会成为老男孩</a>|
-                                </li>
-                                <li>
-                                    <a href='javascript:void(0);'>D350</a>|
-                                </li>
-                                <li>
-                                    <a href='javascript:void(0);'>大邳</a>|
-                                </li>
-                                <li>
-                                    <a href='javascript:void(0);'>站长之家</a>
-                                </li>
-                                <li>
-                                    <a href='admin/index/login.html'>管理员</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class='banndbb'>
-                            Powered by Sanbing v1.0.0 &copy;2017 伞兵一队
-                        </div>
-                    </div>
-                </div>
-                <div id='clear'></div>
-        </div>
+        <div id='clear'></div>
+    </div>
   </div>
 </template>
 
 <script>
-import footImg from './assets/index/foot.png'
-export default {
-  name: 'App',
-  date () {
-    return {
-      footImg: footImg
-    }
-  }
-}
 </script>
 
 <style>
 *{
-  margin: 0;
-  padding: 0;
-  list-style:none;
+  margin : 0;
+  padding : 0;
+  list-style : none;
+text-decoration : none;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -123,15 +129,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 1140px;
+  margin: 0 auto;
   /* margin-top: 60px; */
 }
 /* 头部的样式 */
 .header {
-    width: 1140px;
     height: 80px;
-    margin: 0 auto;
-    /* background-color: aquamarine; */
-    /* background: url("head.png") no-repeat; */
+    background-color: aquamarine;
+    background: url("/static/index/head.png") no-repeat;
     background-size: 100%;
 }
 /* 头部左边导航栏 */
@@ -167,11 +173,22 @@ export default {
 .header .header-left li:hover{
     background-color: #f0f1f2;
 }
+.header .header-right{
+    float: right;
+    margin-top: 19px;
+}
+.header .header-right .subZhu li{
+    float: left;
+   
+}
 .header .header-right .subZhu li a {
     display: block;
     height: 40px;
     width: 50px;
     line-height: 40px;
+    background-color: #ED7D31;
+    border-radius: 50%;
+    border: 1px solid #ED7D31;
 }
 
 .header .header-right .subZhu li a:active {
@@ -180,6 +197,12 @@ export default {
 
 .header .header-right .subZhu li:nth-child(2) {
     margin-right: 5px;
+    margin-left: 10px;
+}
+
+.content{
+    height: 500px;
+    background-color: antiquewhite;
 }
 /* 底部样式 */
 .bottom {
@@ -258,7 +281,7 @@ export default {
 
 .bottom .bannerd .bannerdt .bant3 .guanzhu img {
     width: 127px;
-    margin-top: -37px;
+    margin-top: -35px;
     margin-left: 40px;
 }
 
